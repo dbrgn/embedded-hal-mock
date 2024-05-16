d9f58fc88f58b31f1cf8552db4e332339622f23b
Danilo Bargen <mail@dbrgn.ch>
2024-05-16 21:53:41 +0200

    Improve top level usage docs

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,24 @@
 //!
 //! ## Usage
 //!
-//! See module-level docs for more information.
+//! The general approach for testing drivers using mocks:
 //!
-//! ## embedded_hal version
+//! 1. Define the expectations: A list of transactions (e.g. a read or write
+//!    operation) that you expect the driver-under-test to invoke on the mocked
+//!    hardware
+//! 2. Instantiate the mock with the expectations
+//! 3. Run the test code
+//! 4. At the end of the test code, call the `.done()` method on the mock to
+//!    ensure that all expectations were met
+//!
+//! For more information, see module-level docs.
+//!
+//! **Note:** Mocks contain an `Arc` internally and can be cloned freely. This
+//! means you can clone a mock before passing it to the driver, and then call
+//! `.done()` on the second mock instance without having to reclaim the first
+//! instance from the driver.
+//!
+//! ## embedded_hal Version Support
 //!
 //! This crate supports both version 0.x and version 1.x of embedded-hal.  By default only support
 //! for version 0.x is enabled.  To enable support for version 1.x, use the `eh1` feature.
